18225964375002fad8fd5e53b564fe6df2ef64c0
Akanov-Kanta <129672907+Akanov-Kanta@users.noreply.github.com>
2023-06-06 12:30:53 +0600

    gg

--- lib/const/constants.dart
+++ lib/const/constants.dart
@@ -1,1 +1,8 @@
 import 'package:flutter/material.dart';
+const Color DarkPurple = Color.fromRGBO(72, 56, 209, 1);
+const Color TransRed = Color.fromRGBO(247, 122, 85, 0.45);
+const Color TransGreen = Color.fromRGBO(175, 247, 85, 0.45);
+const Color TransOrange = Color.fromRGBO(247, 122, 85, 1);
+
+
+
