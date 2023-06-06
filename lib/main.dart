91d7a3f7288d882da209c57c11f7ca6ebfaba563
chillit <121033253+chillit@users.noreply.github.com>
2023-06-07 00:20:34 +0600

    appbar

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,13 @@ import 'package:courses/pages/LoginPage.dart';
 import 'package:courses/pages/courses/all_topics.dart';
 import 'package:courses/pages/courses/topic_courses.dart';
 import 'package:flutter/material.dart';
+import 'package:courses/pages/schedule.dart';
 
 void main() {
   runApp( MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Courses NIS PTR",
-      home: LoginPage(),
+      home: schedule(),
     // Login()
   ));
 }
