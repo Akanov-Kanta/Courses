54c3bb048fe7e91415bb0e0a1f23f364a8d2e572
chillit <121033253+chillit@users.noreply.github.com>
2023-06-15 14:20:22 +0600

    take roles from auth

--- lib/Auth.dart
+++ lib/Auth.dart
@@ -1,26 +1,61 @@
+import 'package:courses/main.dart';
 import 'package:courses/pages/main_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:courses/pages/LoginPage.dart';
-import 'package:courses/pages/courses/all_topics.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:courses/pages/courses/teacherCourses.dart';
 
-class AuthPage extends StatelessWidget {
+class AuthPage extends StatefulWidget {
   const AuthPage({super.key});
 
+  @override
+  State<AuthPage> createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: StreamBuilder<User?>(
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
-          // user is logged in
           if (snapshot.hasData) {
-            return MainPage();
-          }
+            User? user = FirebaseAuth.instance.currentUser;
+            String? userId = user?.uid;
 
-          // user is NOT logged in
-          else {
+            return StreamBuilder(
+              stream: FirebaseDatabase.instance.ref().child('users').child(userId!).onValue,
+              builder: (context, snapshot) {
+                if (snapshot.hasData && snapshot.data!.snapshot.value != null) {
+                  dynamic userData = snapshot.data!.snapshot.value;
+                  String? role = userData['role'];
+                  print(role);
+
+                  if (role == 'Student') {
+                    userRole = Roles.student;
+                  } else if (role == 'Teacher') {
+                    userRole = Roles.teacher;
+                  } else if (role == 'Admin') {
+                    userRole = Roles.admin;
+                  }
+                  return MainPage();
+
+                } else {
+                  return Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        CircularProgressIndicator(),
+                      ],
+                    )
+                  );
+                }
+              },
+            );
+          } else {
             return LoginPage();
           }
         },
