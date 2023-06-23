a218e8f2256907782a6080f9b5053c250f515f3d
RandomnieBukvi <117274482+RandomnieBukvi@users.noreply.github.com>
2023-06-23 23:27:39 +0600

    createnewcourse upgrade
    нельзя вписать ничего, кроме цифр в максимальное количество учеников. Нормальная установка времени начала и конца курса. Проверка на заполненность всех полей

--- lib/main.dart
+++ lib/main.dart
@@ -7,28 +7,32 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:courses/Auth.dart';
 import 'package:courses/pages/users_page.dart';
 
-enum Roles {student, teacher, admin}
+enum Roles { student, teacher, admin }
+
 Roles userRole = Roles.admin;
 
-Future main() async{
+Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
       options: FirebaseOptions(
-        apiKey: "AIzaSyBgIMiDfWszcjDrUOlFLQx7zmWP-52iywo",
-        authDomain: "coursesnis-87b8f.firebaseapp.com",
-        databaseURL: "https://coursesnis-87b8f-default-rtdb.europe-west1.firebasedatabase.app",
-        projectId: "coursesnis-87b8f",
-        storageBucket: "coursesnis-87b8f.appspot.com",
-        messagingSenderId: "402293224745",
-        appId: "1:402293224745:web:da93d0c8695bdd3c027338",
-        measurementId: "G-5P6MFPC2ZV",
-      ));
-  runApp( MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Courses NIS PTR",
-      theme: ThemeData(fontFamily: 'Poppins'),
-      home: AuthPage(),
+    apiKey: "AIzaSyBgIMiDfWszcjDrUOlFLQx7zmWP-52iywo",
+    authDomain: "coursesnis-87b8f.firebaseapp.com",
+    databaseURL:
+        "https://coursesnis-87b8f-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "coursesnis-87b8f",
+    storageBucket: "coursesnis-87b8f.appspot.com",
+    messagingSenderId: "402293224745",
+    appId: "1:402293224745:web:da93d0c8695bdd3c027338",
+    measurementId: "G-5P6MFPC2ZV",
+  ));
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "Courses NIS PTR",
+    theme: ThemeData(
+      fontFamily: 'Poppins',
+      colorSchemeSeed: DarkPurple,
+    ),
+    home: AuthPage(),
     // Login()
   ));
 }
-
--- lib/pages/courses/createnewcourse.dart
+++ lib/pages/courses/createnewcourse.dart
@@ -1,6 +1,8 @@
+import 'package:courses/pages/LoginPage.dart';
 import 'package:courses/pages/courses/createnewchapter.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CreateNewCourse extends StatefulWidget {
   CreateNewCourse({Key? key, required this.topicName}) : super(key: key);
@@ -77,6 +79,7 @@ class _CreateNewCourseState extends State<CreateNewCourse> {
     super.dispose();
   }
 
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     if (MediaQuery.of(context).size.width > 600) {
@@ -134,126 +137,118 @@ class _CreateNewCourseState extends State<CreateNewCourse> {
             Expanded(
               child: SingleChildScrollView(
                 padding: EdgeInsets.all(10),
-                child: Column(
-                  children: [
-                    CustomInputField(
-                        texting: 'Введите название курса',
-                        controller: _courseName),
-                    CustomInputField(
-                        texting: 'Введите ФИО учителя',
-                        controller: _courseTeacher),
-                    CustomInputField(
-                        texting: 'Введите кабинет', controller: _courseCabinet),
-                    CustomInputField(
-                        texting: 'Введите максимальное количество участников',
-                        controller: _courseAmount),
-                    ...timeTileList,
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      mainAxisSize: MainAxisSize.max,
-                      children: [
-                        IconButton(
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      CustomInputField(
+                          texting: 'Введите название курса',
+                          controller: _courseName),
+                      CustomInputField(
+                          texting: 'Введите ФИО учителя',
+                          controller: _courseTeacher),
+                      CustomInputField(
+                          texting: 'Введите кабинет',
+                          controller: _courseCabinet),
+                      CustomInputField(
+                          texting: 'Введите максимальное количество участников',
+                          numbersOnly: true,
+                          controller: _courseAmount),
+                      ...timeTileList,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          IconButton(
+                              onPressed: () {
+                                add();
+                              },
+                              icon: Icon(Icons.add)),
+                          IconButton(
+                              onPressed: () {
+                                remove();
+                              },
+                              icon: Icon(Icons.remove)),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 30),
+                        child: SizedBox(
+                          width: double.infinity,
+                          height: 50,
+                          child: ElevatedButton(
                             onPressed: () {
-                              add();
-                            },
-                            icon: Icon(Icons.add)),
-                        IconButton(
-                            onPressed: () {
-                              remove();
-                            },
-                            icon: Icon(Icons.remove)),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 30),
-                      child: SizedBox(
-                        width: double.infinity,
-                        height: 50,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            print('''_courseAmount.text.isNotEmpty: ${_courseAmount.text.isNotEmpty} \n
-                                _courseCabinet.text.isNotEmpty: ${_courseCabinet.text.isNotEmpty} \n
-                                _courseName.text.isNotEmpty: ${_courseName.text.isNotEmpty} \n
-                                _courseTeacher.text.isNotEmpty: ${_courseTeacher.text.isNotEmpty} \n
-                                timeListOptions
-                                    .every((element) => element != null): ${timeListOptions
-                                    .every((element) => element != null)} \n
-                                timeListControllers.every(
-                                    (element) => element.text.isNotEmpty): ${timeListControllers.every(
-                                    (element) => element.text.isNotEmpty)} \n''');
-                            print(timeListOptions);
-                            print(timeListControllers.map((e) => e.text).toList());
-                            if (_courseAmount.text.isNotEmpty &&
-                                _courseCabinet.text.isNotEmpty &&
-                                _courseName.text.isNotEmpty &&
-                                _courseTeacher.text.isNotEmpty &&
-                                timeListOptions
-                                    .every((element) => element != null) &&
-                                timeListControllers.every(
-                                    (element) => element.text.isNotEmpty)) {
-                              showDialog(
-                                  context: context,
-                                  builder: (context) {
-                                    return ConfirmationDialog(
-                                      title:
-                                          'Вы точно хотите создать новый курс?',
-                                      onConfirm: () async {
-                                        var ref =
-                                            FirebaseDatabase.instance.ref();
-                                        Map schedule = Map.fromIterables(
-                                            timeListOptions,
-                                            timeListControllers
-                                                .map((e) => e.text)
-                                                .toList());
-                                        ref
-                                            .child(
-                                                'courses/${_courseName.text}')
-                                            .set({
-                                          'cabinet': _courseCabinet.text,
-                                          'max': int.parse(_courseAmount.text),
-                                          'schedule': schedule,
-                                          'teacher': _courseTeacher.text,
-                                        });
-                                        var buf = await ref.child(
-                                                'topics/${widget.topicName}/courses').get();
-                                        print('buf.value.runtimeType: ${buf.value.runtimeType}; topic name: ${widget.topicName}; buf.value: ${buf.value}');
-                                        int num = buf.value == null ? 0 : (buf.value as List).length;
+                              if (_formKey.currentState!.validate()) {
+                                showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      return ConfirmationDialog(
+                                        title:
+                                            'Вы точно хотите создать новый курс?',
+                                        onConfirm: () async {
+                                          var ref =
+                                              FirebaseDatabase.instance.ref();
+                                          Map schedule = Map.fromIterables(
+                                              timeListOptions,
+                                              timeListControllers
+                                                  .map((e) => e.text)
+                                                  .toList());
+                                          ref
+                                              .child(
+                                                  'courses/${_courseName.text}')
+                                              .set({
+                                            'cabinet': _courseCabinet.text,
+                                            'max':
+                                                int.parse(_courseAmount.text),
+                                            'schedule': schedule,
+                                            'teacher': _courseTeacher.text,
+                                          });
+                                          var buf = await ref
+                                              .child(
+                                                  'topics/${widget.topicName}/courses')
+                                              .get();
+                                          print(
+                                              'buf.value.runtimeType: ${buf.value.runtimeType}; topic name: ${widget.topicName}; buf.value: ${buf.value}');
+                                          int num = buf.value == null
+                                              ? 0
+                                              : (buf.value as List).length;
 
-                                        ref
-                                            .child(
-                                                'topics/${widget.topicName}/courses')
-                                            .update({
-                                              num.toString(): _courseName.text,
-                                            });
-                                        Navigator.of(context).pop();
-                                      },
-                                    );
-                                  });
-                            }
-                          },
-                          child: Text(
-                            'Создать',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontFamily: 'Futura',
-                              fontSize: 19,
+                                          ref
+                                              .child(
+                                                  'topics/${widget.topicName}/courses')
+                                              .update({
+                                            num.toString(): _courseName.text,
+                                          });
+                                          Navigator.of(context).pop();
+                                        },
+                                      );
+                                    });
+                              }
+                            },
+                            child: Text(
+                              'Создать',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: 'Futura',
+                                fontSize: 19,
+                              ),
                             ),
-                          ),
-                          style: ElevatedButton.styleFrom(
-                            primary: Colors
-                                .black, // Replace with your desired button color
-                            elevation: 3,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8),
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors
+                                  .black, // Replace with your desired button color
+                              elevation: 3,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8),
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
@@ -267,18 +262,23 @@ class _CreateNewCourseState extends State<CreateNewCourse> {
 class CustomInputField extends StatelessWidget {
   final String texting;
   final TextEditingController controller;
-
+  final void Function()? onTap;
+  final bool? readOnly;
+  final bool? numbersOnly;
   const CustomInputField({
     Key? key,
     required this.texting,
     required this.controller,
+    this.onTap,
+    this.readOnly,
+    this.numbersOnly,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      height: 100,
+      height: 120,
       child: Column(
         children: [
           SizedBox(
@@ -298,6 +298,15 @@ class CustomInputField extends StatelessWidget {
           Padding(
             padding: EdgeInsetsDirectional.fromSTEB(20, 0, 20, 0),
             child: TextFormField(
+              readOnly: readOnly ?? false,
+              keyboardType: numbersOnly != null && numbersOnly == true
+                  ? TextInputType.number
+                  : null,
+              inputFormatters: numbersOnly != null && numbersOnly == true
+                  ? [
+                      FilteringTextInputFormatter.digitsOnly,
+                    ]
+                  : null,
               validator: (value) {
                 if (value == null || value.isEmpty) {
                   return 'Заполните это поле';
@@ -335,6 +344,7 @@ class CustomInputField extends StatelessWidget {
                 fontSize: 18,
                 fontWeight: FontWeight.normal,
               ),
+              onTap: onTap,
             ),
           ),
         ],
@@ -363,7 +373,7 @@ class CustomDropDown extends StatelessWidget {
     defaultOptions = List.from(options);
     return Container(
       width: double.infinity,
-      height: 100,
+      height: 120,
       child: Column(
         children: [
           SizedBox(height: 5),
@@ -376,6 +386,12 @@ class CustomDropDown extends StatelessWidget {
           Padding(
             padding: EdgeInsetsDirectional.fromSTEB(20, 0, 20, 0),
             child: DropdownButtonFormField<String>(
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Заполните это поле';
+                }
+                return null;
+              },
               value: selectedOption,
               onChanged: (String? newValue) {
                 if (selectedOption != null)
@@ -455,8 +471,58 @@ class TimeTile extends StatelessWidget {
           },
         ),
         CustomInputField(
-            texting: 'Введите время на день',
-            controller: timeListControllers.elementAt(index)),
+          texting: 'Введите время на день',
+          controller: timeListControllers.elementAt(index),
+          readOnly: true,
+          onTap: () async {
+            final time = await showTimePicker(
+              context: context,
+              initialTime: TimeOfDay(hour: 16, minute: 0),
+              initialEntryMode: TimePickerEntryMode.input,
+              helpText: 'Время начала',
+              cancelText: 'Отмена',
+              confirmText: 'Дальше',
+              hourLabelText: 'Часы',
+              minuteLabelText: 'Минуты',
+              errorInvalidText: 'Введите корректное время',
+              builder: (context, child) {
+                return MediaQuery(
+                    data: MediaQuery.of(context)
+                        .copyWith(alwaysUse24HourFormat: true),
+                    child: child!);
+              },
+            );
+            if (time == null) {
+              return;
+            }
+
+            await Future.delayed(Duration(milliseconds: 150));
+
+            final time2 = await showTimePicker(
+              context: context,
+              initialTime: TimeOfDay(hour: 16, minute: 0),
+              initialEntryMode: TimePickerEntryMode.input,
+              helpText: 'Время окончания',
+              cancelText: 'Отмена',
+              confirmText: 'Готово',
+              hourLabelText: 'Часы',
+              minuteLabelText: 'Минуты',
+              errorInvalidText: 'Введите корректное время',
+              builder: (context, child) {
+                return MediaQuery(
+                    data: MediaQuery.of(context)
+                        .copyWith(alwaysUse24HourFormat: true),
+                    child: child!);
+              },
+            );
+            if (time2 == null) {
+              return;
+            }
+
+            timeListControllers.elementAt(index).text =
+                '${time.hour.toString().padLeft(2, '0')}:${time.minute.toString().padLeft(2, '0')}-${time2.hour.toString().padLeft(2, '0')}:${time2.minute.toString().padLeft(2, '0')}';
+          },
+        ),
       ],
     );
   }
